2a8f064e33a9c4b23549d4c6560c41bb41712bdf
Art4es <Art4es@gmail.com>
2019-08-30 13:36:30 +0300

    Add DB connection

--- config/db.php
+++ config/db.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once __DIR__.'/db_cfg.php';
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host='.DB_HOST.';dbname='.DB_NAME,
+    'username' => DB_USER,
+    'password' => DB_PASSWORD,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
